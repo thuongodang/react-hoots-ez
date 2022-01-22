1258d0039e8c8fc0d355bde8e2e15a7d2c81c1d2
THAO <thao@MacBook-Air-cua-THAO.local>
2022-01-23 03:30:38 +0700

    add color box example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import ColorBox from './component/colorBox';
 
 function App() {
   return (
     <div className="app">
       <h1>Welcome to react Hooks!</h1>
+      <ColorBox />
     </div>
   );
 }
