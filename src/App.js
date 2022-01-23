f2616a9c8ae5c2a41f58145f998f8654407afeba
THAO <thao@MacBook-Air-cua-THAO.local>
2022-01-23 14:12:16 +0700

    add todo list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import './App.scss';
-import ColorBox from './component/colorBox';
+import React, { useState } from "react";
+import "./App.scss";
+import ColorBox from "./component/colorBox";
+import TodoList from "./component/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "I learn ReactJs! X" },
+    { id: 2, title: "We learn ReactJs! X" },
+    { id: 3, title: "they learn ReactJs! X" },
+  ]);
+
+  function handleTodoClick(todo){
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if(index < 0) return;
+
+    const newTodolist = [...todoList];
+    newTodolist.splice(index, 1);
+    setTodoList(newTodolist);
+    
+  }
+
   return (
     <div className="app">
-      <h1>Welcome to react Hooks!</h1>
-      <ColorBox />
+      <h1>React hooks - Todolist</h1>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick}/>
     </div>
   );
 }
